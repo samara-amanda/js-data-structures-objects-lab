9d634db9f6c9e5afbf5fcf9535cbf6c1f1ff19a0
Sam <samara.amanda01@gmail.com>
2021-04-02 02:58:29 -0400

    commit tree d8c5edf13cc1592f7aa5490b06b01935ab171213

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const driver = {
+    name: 'Sam'
+};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = {...driver};
+    newDriver[key] = value;
+    return newDriver;
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver;
+};
+
+function deleteFromDriverByKey(driver, key) {
+    let newDriver = {...driver}
+    delete newDriver[key]
+    return newDriver
+};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+};
